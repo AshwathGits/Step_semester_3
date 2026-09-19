078745a3ec60936aa7d8a3f51b2b0c03f7af24b4
AshwathGits <ashwathachuuu@gmail.com>
2026-09-19 23:35:39 +0530

    Add M5 Reverse Customer Name

---
+++ src/main/java/week1/class_problems/ReverseCustomerName.java
@@ -0,0 +1,25 @@
+package week1.class_problems;
+
+public class ReverseCustomerName {
+
+    public static String reverseCustomerName(String customerName) {
+
+        String reversedName = "";
+
+        for (int i = customerName.length() - 1; i >= 0; i--) {
+            reversedName += customerName.charAt(i);
+        }
+
+        return reversedName;
+    }
+
+    public static void main(String[] args) {
+
+        String customerName = "Sunil";
+
+        String reversedName = reverseCustomerName(customerName);
+
+        System.out.println("Original Name: " + customerName);
+        System.out.println("Reversed Name: " + reversedName);
+    }
+}
